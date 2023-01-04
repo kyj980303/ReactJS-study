412779f039086c4c855bb432eaad4fa021cda8a3
YJung <rladuswjd0303@naver.com>
2023-01-04 22:46:38 +0900

    separate react page

--- make-react-app/src/index.js
+++ make-react-app/src/index.js
@@ -4,11 +4,13 @@ import App from "./App";
 import App2 from "./App2";
 import Practice from "./Practice";
 import ConinTracket from "./ConinTracket";
+import MovieApp from "./MovieApp";
 
 ReactDOM.render(
   // <App />,
   // <App2 />,
   // <Practice />,
-  <ConinTracket />,
+  // <ConinTracket />,
+  <MovieApp />,
   document.getElementById("root")
 );
